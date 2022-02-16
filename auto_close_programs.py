668b133e3e488f20b76e3f237939e6a60c411b65
Stefan Schoerkmeier <53307237+Steveineiter@users.noreply.github.com>
2022-02-16 09:39:00 +0100

    Add files via upload

---
+++ auto_close_programs.py
@@ -0,0 +1,38 @@
+######################################
+# My First usefull script lol
+# Needs to be added in autostart, is closing programs specifiec in PROGRAMS_TO_CLOSE
+# 
+# Author: Stefan Schoerkmeier
+# Date: 2/16/2022
+######################################
+import psutil,os
+from datetime import datetime, time
+
+
+# ====================== Tweakable Parameters ======================
+PROGRAMS_TO_CLOSE = ["steam.exe"]
+CLOSING_DAYS = [0, 1, 2, 3, 4] # 0 = monday, 1 = tuesday ... according to datetime.today().weekday()
+CLOSING_TIME_FRAMES = [(time(5,00), time(11,00)), (time(20,00), time(23,00))] 
+
+# ====================== Helper Methods ============================
+def is_in_closing_time():
+    current_time = datetime.now().strftime("%H:%M:%S")
+
+    for start_time, end_time in CLOSING_TIME_FRAMES:
+        if start_time < end_time: 
+            is_in_time_frame = current_time >= start_time and current_time <= end_time 
+        else: 
+            #Over midnight: 
+            is_in_time_frame = current_time >= start_time or current_time <= end_time 
+
+        if is_in_time_frame:
+            return is_in_time_frame
+
+# ====================== Main Program =============================
+current_weekday = datetime.today().weekday()
+if current_weekday in CLOSING_DAYS:
+    if is_in_closing_time(): 
+        for process in (process for process in psutil.process_iter() if process.name() in PROGRAMS_TO_CLOSE):
+            process.kill()
+
+
